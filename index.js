d4d412f8d5174ed0bde558cba2a69861c5d1bc36
wendidy <helianyeshui@gmail.com>
2020-04-19 19:58:23 -0400

    making my bot online yayyy

--- index.js
+++ index.js
@@ -1,3 +1,9 @@
 const Discord = require("discord.js");
 const bot = new Discord.Client();
-const token = "";
+const token = "NzAxMTQ3NDI0NTY3MTk3Nzk4.XpxsKw.VBcdBjKA_L4I0R2KGG-2uNVPkLA";
+
+bot.on("ready", () => {
+  console.log("Tis Bot is online, yayyy");
+});
+
+bot.login(token);
