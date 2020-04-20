7ed26b778d1f1f056baaee3c73d7ebe9caacb2c8
wendidy <helianyeshui@gmail.com>
2020-04-19 21:03:04 -0400

    making the bot repond to multiple hi's and hello's, kind of dumb but it makes me happy

--- index.js
+++ index.js
@@ -1,13 +1,14 @@
 const Discord = require("discord.js");
 const bot = new Discord.Client();
 const token = "NzAxMTQ3NDI0NTY3MTk3Nzk4.XpxsKw.VBcdBjKA_L4I0R2KGG-2uNVPkLA";
+const PREFIX = "#";
 
 bot.on("ready", () => {
   console.log("Tis Bot is online, yayyy");
 });
 
 bot.on("message", (m) => {
-  if (m.content === "HELLO") {
+  if (m.content.toUpperCase() === "HELLO" || m.content.toUpperCase() === "HI") {
     m.reply("Wow you are not a bot, you talk?");
   }
 });
