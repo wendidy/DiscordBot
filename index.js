f8a3206940ef911a53a8184c1b0cd068cc69f69c
wendidy <helianyeshui@gmail.com>
2020-04-20 13:05:06 -0400

    I tried so hard to make the NaN comparison work, converting from string to number etc. The comparison operations in js is quite interesting and annoying at the same time

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const Discord = require("discord.js");
 const bot = new Discord.Client();
 const token = "NzAxMTQ3NDI0NTY3MTk3Nzk4.Xpz70w.3FAnszxU8osgyJexzWNaSMZW9mI";
-const PREFIX = "#";
+const PREFIX = ".";
 
 bot.on("ready", () => {
   console.log("Tis Bot is online, yayyy");
@@ -9,12 +9,13 @@ bot.on("ready", () => {
 
 bot.on("message", (m) => {
   //checking if the first char is '#'
-  if (m.content[0] != "#") {
+  if (m.content[0] != ".") {
     return;
   }
   //length will always be 1, basically its splitting the string into words separated by space
   //meanwhile get rid of the prefix
-  let args = m.content.substring(PREFIX.length).split(" ");
+  let args = m.content.slice(PREFIX.length).split(" ");
+  //console.log(args);
   switch (args[0]) {
     // case t.toUpperCase() === "hi".toUpperCase(): //need to fix this
     //   m.reply("hi");
@@ -40,9 +41,26 @@ bot.on("message", (m) => {
       break;
     case "clear":
       if (!args[1]) return m.reply("please specify");
-      else if (args[1] <= 0)
-        return m.reply("invalid number, number needs to be > 0");
-      m.channel.bulkDelete(args[1]); //in this case, args[1] has to be an int
+      //console.log(typeof args[1]); //returns a string ****
+      else if (Number(args[1]) != Number(args[1])) {
+        return m.reply("invalid");
+      } else if (args[1] <= 0) {
+        return m.reply("What do you think I am, put a positive number!");
+      } else {
+        m.channel.bulkDelete(args[1]); //in this case, args[1] has to be an int
+      }
+      //console.log("hi");
+      break;
+    case "embed":
+      const embed = new Discord.MessageEmbed()
+        .addField("Name", m.author.username, true)
+        .addField("Current server", m.guild.name, true)
+        .setTitle("Info Embed")
+        .setColor(0xd03716)
+        .setFooter("So this is a footer")
+        .setURL("https://discord.js.org/#/docs/main/stable/general/welcome")
+        .setThumbnail(m.author.displayAvatarURL());
+      m.channel.send(embed);
       break;
   }
   // if (m.content.toUpperCase() === "HELLO" || m.content.toUpperCase() === "HI") {
